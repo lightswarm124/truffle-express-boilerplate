8d12de55a8d363af518544a3787bf2b9ca3e9560
lightswarm124 <lightswarm@gmail.com>
2018-09-17 16:52:48 -0400

    started on JWT

---
+++ src/services/JWTBlockchainPayload.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+
+const wrap = require('../../middlewares/wrap');
+
+const cert = 'bitcoincash:qz8wl7reul0z8sxp9h7hyxduhq6cvfllksltczkkgp';
+
+module.exports = async () => {
+  let blockHeight = await axios.get('https://rest.bitcoin.com/v1/blockchain/getBlockCount')
+    .then(result => {
+      return result.data;
+    })
+    .catch(err => {
+      return err;
+    });
+
+  let merkleRoot = await axios.get(`https://rest.bitcoin.com/v1/block/details/${blockHeight}`)
+    .then(result => {
+      return result.data.merkleroot;
+    })
+    .catch(err => {
+      return err;
+    });
+
+  let payload = await {
+    bkh: blockHeight,
+    mkr: merkleRoot,
+    ebn: blockHeight + 1
+  }
+
+  return jwt.sign(payload, cert, { algorithm: 'HS256', noTimestamp: true }, function(err, token) {
+    if (!err) {
+      console.log(token);
+    } else {
+      console.log(err);
+    }
+  });
+}
---
+++ test/jwt.test.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const request = require('supertest');
+
+let jwtTest = require('./JWTBlockchainPayload');
+
+const cert = 'bitcoincash:qz8wl7reul0z8sxp9h7hyxduhq6cvfllksltczkkgp';
+
+describe('jwt', () => {
+  it('returns a jwt token', () => {
+    
+  });
+});
