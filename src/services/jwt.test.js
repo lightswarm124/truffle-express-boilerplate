b786a2617de9b60d29a87c98d0d713649c4a7478
lightswarm124 <lightswarm@gmail.com>
2018-09-21 16:42:35 -0400

    fixed merkleRoot check

--- src/services/jwt.test.js
+++ src/services/jwt.test.js
@@ -5,28 +5,19 @@ const axios = require('axios');
 
 let jwtTest = require('./JWTBlockchainPayload');
 
-let validation;
-
-/*
-async function testJWTToken() {
-  let signedData = await jwtTest();
-  await console.log('\nJWT Token: ', signedData);
-  let decodeMessage = await jwt.decode(signedData, { complete: true });
-  await console.log('\nDecode JWT: ', decodeMessage);
-  let validation = await jwt.verify(signedData, server, { ignoreExpiration: true });
-  await console.log('\nValidation: ', validation);
-}
-*/
-
-//setInterval(() => {
-//testJWTToken();
-testAuthentication();
-//}, 30000);
-
 async function testAuthentication() {
+  let validation;
   let blockHeight = await axios.get('https://rest.bitcoin.com/v1/blockchain/getBlockCount')
     .then(result => {
       return result.data;
+    })
+    .catch(err => {
+      return err;
+    });
+
+  let merkleRoot = await axios.get(`https://rest.bitcoin.com/v1/block/details/${blockHeight}`)
+    .then(result => {
+      return result.data.merkleroot;
     })
     .catch(err => {
       return err;
@@ -37,8 +28,9 @@ async function testAuthentication() {
   await console.log(decodeMessage);
   let JWTbkh = await decodeMessage.payload.bkh;
   let JWTebn = await decodeMessage.payload.ebn;
+  let JWTmkr = await decodeMessage.payload.mkr;
 
-  if (JWTbkh === blockHeight && JWTebn > blockHeight) {
+  if (JWTbkh === blockHeight && JWTmkr === merkleRoot && JWTebn > blockHeight) {
     try {
       let validation = jwt.verify(signedData, process.env.SECRET, { ignoreExpiration: true });
       console.log('Signature Authentication Successful');
@@ -49,3 +41,5 @@ async function testAuthentication() {
     console.log('Invalid Token Provided');
   }
 }
+
+testAuthentication();
