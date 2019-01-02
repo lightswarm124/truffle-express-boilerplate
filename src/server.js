dde5055d54c0fcd871458d9b6fe79e2027110c2a
lightswarm124 <lightswarm@gmail.com>
2019-01-02 14:13:38 -0500

    minor express server changes

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(helmet());
